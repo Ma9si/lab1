54f8f5484ee7b2552568c952469248d22d754a65
Mansi Saini <mansisaini848@gmail.com>
2019-02-10 22:23:25 +0530

    second commit

--- calc.c
+++ calc.c
@@ -1,12 +1,21 @@
 #include<stdio.h>
 int main()
 {
-int a,b,op;
+int a,b;char op;
 printf("Enter two numbers : ");
 scanf("%d%d",&a,&b);
 printf("\nEnter operation ");
-scanf("%d",&op);
-
+scanf("%c",&op);
+switch(op)
+{
+	case '+':
+		printf("%d",a+b);
+		break;
+	case '-':
+		printf("%d",a-b);
+		break;
+	
+}
 
 return 0;
 }
